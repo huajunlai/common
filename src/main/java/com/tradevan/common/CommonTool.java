233d9c40f3fe306dd26eb471e31b8debd20f1d7f
huajun.lai <huajun.lai@tradevan.com.tw>
2019-07-19 16:28:17 +0800

    init commit

---
+++ src/main/java/com/tradevan/common/CommonTool.java
@@ -0,0 +1,15 @@
+package com.tradevan.common;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Hello world!
+ *
+ */
+@Component
+public class CommonTool 
+{
+    public String getText() {
+        return "test";
+    }
+}
